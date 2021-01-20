3142d038b8a4f800bb82261eb0eaef562229875f
JessicaPrior <jsscprior@gmail.com>
2021-01-20 17:49:55 -0300

    Teste da aplicaçao

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use("/user", userRouter);
 app.use("/band", bandRouter)
 app.use("/show", showRouter)
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 5000, () => {
     if (server) {
       const address = server.address() as AddressInfo;
       console.log(`Servidor rodando em http://localhost:${address.port}`);
